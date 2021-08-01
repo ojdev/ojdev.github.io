730d4d21927c67128c1d0c22d86b8ccab87cef7d
ojdev <ojdev@users.noreply.github.com>
2021-08-01 08:23:06 +0000

    deploy: cd38c370c0ffa2545c4ccb73b3f55138e219a1f3

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,7 +1,9 @@
-'use strict';
-
-el = document.querySelector("article.page-content");
-
-if (el !== undefined) {
-    el.innerText ='测试注入';
-}
+(function () {
+    el = document.querySelector("article.page-content");
+    if (el !== undefined) {
+        el.innerText = '测试注入';
+    }
+    //   posts[0].innerHTML = '<div class="note note-warning" style="font-size:0.9rem"><p>' +
+    //     '<div class="h6">文章时效性提示</div><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。' +
+    //     '</p></p></div>' + posts[0].innerHTML;
+})();
