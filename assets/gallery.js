635b448058d74f2d2cb3411d371b1f2bc03cb173
ojdev <ojdev@users.noreply.github.com>
2021-08-01 10:33:02 +0000

    deploy: 87c269b9e7ffa46557ace3c5475f38816ce64ae9

--- assets/gallery.js
+++ assets/gallery.js
@@ -5,15 +5,15 @@
             .then(response => response.json())
             .then(data => {
                 data.forEach(d => {
-                    el.innerText += d.type+'&nbsp;&nbsp;'+d.name+'<br />';
-                    if(d.contents!==undefined&&d.contents.length>0){
-                        d.contents.forEach(sd=>{
-                            el.innerText += d.type+'&nbsp;&nbsp;'+d.name+'<br />';
+                    el.innerHTML += d.type + '&nbsp;&nbsp;' + d.name + '<br />';
+                    if (d.contents !== undefined && d.contents.length > 0) {
+                        d.contents.forEach(sd => {
+                            el.innerHTML += sd.type + '&nbsp;&nbsp;' + sd.name + '<br />';
+                            '<img style="padding: 0; border: none; width: 128px; height:128px;" src="' +  sd.name + '">'
                         });
                     }
                 });
             });
-        el.innerText = '测试注入';
     }
     //   posts[0].innerHTML = '<div class="note note-warning" style="font-size:0.9rem"><p>' +
     //     '<div class="h6">文章时效性提示</div><p>这是一篇发布于 ' + days + ' 天前的文章，部分信息可能已发生改变，请注意甄别。' +
