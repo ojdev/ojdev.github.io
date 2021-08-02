232298778f5fdbf06830dd4077ca3a7c5fee6d37
欧俊 <me@oujun.work>
2021-08-02 16:37:55 +0800

    fix

--- source/assets/gallery.js
+++ source/assets/gallery.js
@@ -1,16 +1,19 @@
 (function () {
-    el = document.querySelector("article.page-content");
+    el = $("article.page-content");
     if (el !== undefined) {
         el.innerHTML = '';
         fetch('data.json')
             .then(response => response.json())
             .then(data => {
+                galleryContent = document.createElement("div");
+                galleryContent.id = "gallery-content";
+                galleryContent.class = "justified-gallery";
                 function renderGallery(node) {
                     if (node.contents !== undefined && node.contents.length > 0) {
                         node.contents.forEach(sd => {
                             imgUrl = node.name + '/' + sd.name;
                             imgThumbUrl = node.name + '/thumbnails/thumb_' + sd.name;
-                            el.innerHTML += `
+                            galleryContent.innerHTML += `
                                 <a  href="`+ imgUrl + `">
                                     <img src="`+ imgThumbUrl + `">
                                 </a>
@@ -18,12 +21,12 @@
                         });
                     }
                 }
+
                 data.forEach(d => {
-                    el.innerHTML += '<div id="gallery-content" class="justified-gallery">'
                     renderGallery(d);
-                    el.innerHTML += '</div>';
                 });
-                document.querySelector('#gallery-content').justifiedGallery({ rowHeight: 70, lastRow: 'nojustify', margins: 3 });
+                el.append(galleryContent);
+                $('#gallery-content').justifiedGallery({ rowHeight: 70, lastRow: 'nojustify', margins: 3 });
             });
     }
 })();
