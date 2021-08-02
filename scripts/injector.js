a26abda37b795fd6257cf74afad761389dc05fa4
欧俊 <me@oujun.work>
2021-08-02 16:15:33 +0800

    瀑布流

--- scripts/injector.js
+++ scripts/injector.js
@@ -1,5 +1,11 @@
+hexo.extend.injector.register('head_end', 
+`
+<link rel="stylesheet" href="/assets/justifiedGallery.min.css" />
+`,
+'gallery')
 hexo.extend.injector.register('body_end', 
 `
+  <script src="/assets/jquery.justifiedGallery.min.js"></script>
   <script src="/assets/gallery.js"></script>
 `,
 'gallery')
