880a89e526a8067e0f47f8af781fadb79bf39122
ojdev <ojdev@users.noreply.github.com>
2021-08-02 08:01:46 +0000

    deploy: 35e0d06d950cfb40a13eaf93eaa3e0df7bc866ab

--- assets/gallery.js
+++ assets/gallery.js
@@ -1,27 +1,27 @@
 (function () {
-    el = document.querySelector("div.markdown-body");
+    el = document.querySelector("article.page-content");
     if (el !== undefined) {
+        el.innerHTML = '';
         fetch('data.json')
             .then(response => response.json())
             .then(data => {
+                el.innerHTML = '<div id="gallery-content" class="justified-gallery">'
                 function renderGallery(node) {
-                    el.innerHTML += node.type + '&nbsp;&nbsp;' + node.name + '<br />';
                     if (node.contents !== undefined && node.contents.length > 0) {
                         node.contents.forEach(sd => {
-                            //el.innerHTML += '<img style="padding: 0; border: none; width: 128px; height:128px;" src="' + node.name + '/thumbnails/thumb_' + sd.name + '">'
                             imgUrl = node.name + '/' + sd.name;
                             imgThumbUrl = node.name + '/thumbnails/thumb_' + sd.name;
-                            el.innerHTML += '<img src="' + imgUrl + '"></img>'
-                            // el.innerHTML += `
-                            //     <a class="fancybox fancybox.image" href="`+ imgUrl + `" itemscope="" itemtype="http://schema.org/ImageObject" itemprop="url" data-fancybox="default" rel="default">
-                            //         <img src="`+ imgThumbUrl + `">
-                            //     </a>
-                            // `;
+                            el.innerHTML += `
+                                <a  href="`+ imgUrl + `">
+                                    <img src="`+ imgThumbUrl + `">
+                                </a>
+                            `;
                         });
-                        //renderGallery(el, node);
                     }
                 }
                 data.forEach(d => renderGallery(d));
+                el.innerHTML += '</div>';
+                $('#gallery-content').justifiedGallery({ rowHeight: 70, lastRow: 'nojustify', margins: 3 });
             });
     }
 })();
